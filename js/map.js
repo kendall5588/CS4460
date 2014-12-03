51f566a79454fe4d29d0ce11ade410b042d40338
Kendall Merritt <kendallmerritt@gmail.com>
2014-12-03 16:11:06 -0500

    trying to get clicking states to work.

--- js/map.js
+++ js/map.js
@@ -3,9 +3,6 @@ var map = {};
 map.draw = function(id, data, tooltip){
 
 	function clicked(d) {
-	
-		// Put actions on state clicked here
-		
 	}
 		
 	function mouseOver(d){
@@ -21,7 +18,7 @@ map.draw = function(id, data, tooltip){
 			.style("top", (d3.event.pageY - 25) + "px");
 	}
 	
-	function mouseOut(){
+	function mouseOut(d){
 	
 		d3.select("#tooltip")
 			.transition()
@@ -35,10 +32,10 @@ map.draw = function(id, data, tooltip){
 	 	.append("path")
 	 	.attr("class", "state")
 	 	.attr("d", function(d){ return d.d;})
-  	    .style("fill", function(d){ return scores[d.id].color; })
+  	    .style("fill", function(d){ return data[d.id].color; })
 	 	.on("mouseover", mouseOver)
 		.on("mouseout", mouseOut)
-		.on("clicked", clicked);
+		.on("click", clicked);
 }
 	
 this.map = map;
